4e9395f9761ff44f332097c3f382ba7fee7e7dfd
Leon Bowie <leon@bowie-co.nz>
2022-04-10 02:08:36 +1200

    fix rps

--- src/commands/Fun/rps.ts
+++ src/commands/Fun/rps.ts
@@ -17,17 +17,14 @@ import { MessageFlags } from 'discord-api-types/v10';
 export class UserCommand extends Command {
 	readonly #options = ['rock', 'paper', 'shears'];
 	public override chatInputRun(_: never, { choice }: { choice: string }): Command.Response {
-		if (!this.#options.includes(choice)) {
-			const content = `You can only choose between ${this.#options.map((option) => hyperlink(option, option)).join(', ')}`;
-			return this.message({ content, flags: MessageFlags.Ephemeral });
-		}
-
 		const botChoice = this.#options[Math.floor(Math.random() * this.#options.length)];
+
 		let content = `I chose ${botChoice} you chose ${choice}.`;
 		if (botChoice === choice) content += "\nIt's a tie!";
 		else if ((this.#options.findIndex((option) => option === botChoice) + 1) % 3 === this.#options.findIndex((option) => option === choice))
 			content += '\nYou win!';
 		else content += '\nI win!';
+
 		return this.message({ content });
 	}
 }
